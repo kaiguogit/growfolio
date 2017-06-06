2430c79a5109b42fec3334c0683f62f23877384e
Kai Guo <git@kaiguo.ca>
2017-06-05 22:53:20 -0700

    Redirect http to https

--- app.js
+++ app.js
@@ -16,6 +16,10 @@ const dotenv = require('dotenv');
 const path = require('path');
 const mongoose = require('mongoose');
 const passport = require('passport');
+const https = require('https');
+const http = require('http');
+const fs = require('fs');
+
 
 /**
  * Load environment variables from .env file, where API keys and passwords are configured.
@@ -37,6 +41,12 @@ const allocationController = require('./controllers/allocation');
  */
 const passportConfig = require('./config/passport');
 
+// SSL keys
+const options = {
+  key: fs.readFileSync('test/fixtures/keys/key.pem'),
+  cert: fs.readFileSync('test/fixtures/keys/cert.pem')
+};
+
 /**
  * Create Express server.
  */
@@ -52,6 +62,18 @@ mongoose.connection.on('error', () => {
   process.exit();
 });
 
+const ensureSecure = (req, res, next) => {
+  // Use 'x-forwarded-proto' for heroku as Heroku terminates SSL connections at the load balancer level
+  // https://stackoverflow.com/questions/32952085/express-js-redirect-to-https-and-send-index-html
+  // https://stackoverflow.com/questions/24015292/express-4-x-redirect-http-to-https
+  if (req.secure || req.headers['x-forwarded-proto'] === 'https') {
+  // OK, continue
+    return next();
+  }
+  res.redirect(`https://${req.headers.host}${req.url}`);
+};
+
+app.all('*', ensureSecure);
 /**
  * Enable CORS
  */
@@ -64,7 +86,7 @@ app.use((req, res, next) => {
 /**
  * Express configuration.
  */
-app.set('port', process.env.PORT || 8000);
+
 app.set('views', path.join(__dirname, 'views'));
 app.use(compression());
 app.use(logger('dev'));
@@ -106,9 +128,30 @@ app.use(errorHandler());
 /**
  * Start Express server.
  */
-app.listen(app.get('port'), () => {
-  console.log('%s App is running at http://localhost:%d in %s mode', chalk.green('✓'), app.get('port'), app.get('env'));
+// https://stackoverflow.com/questions/8449665/how-do-you-detect-the-environment-in-an-express-js-app
+// https://github.com/expressjs/express/blob/master/lib/application.js#L71
+const isDev = app.get('env') === 'development';
+
+// Create an HTTP service.
+// http://expressjs.com/en/api.html
+// https://docs.nodejitsu.com/articles/HTTP/servers/how-to-create-a-HTTPS-server/
+// https://stackoverflow.com/questions/5998694/how-to-create-an-https-server-in-node-js
+// https://stackoverflow.com/questions/8355473/listen-on-http-and-https-for-a-single-express-app
+app.set('httpPort', process.env.PORT || 8000);
+app.set('httpsPort', isDev ? 8001 : 443);
+http.createServer(app).listen(app.get('httpPort'), () => {
+  console.log('%s App is running at http://localhost:%d in %s mode', chalk.green('✓'), app.get('httpPort'), app.get('env'));
   console.log('  Press CTRL-C to stop\n');
 });
+
+// Create an HTTPS service identical to the HTTP service.
+// Can't use this for Heroku as it doesn't allow binding 443
+// It's load balancer will terminates SSL/TLS and give dyno plain HTTP traffic.
+// https://stackoverflow.com/questions/28260492/express-unable-to-bind-to-port-443-on-heroku
+
+// https.createServer(options, app).listen(app.get('httpsPort'), () => {
+//   console.log('%s App is running at https://localhost:%d in %s mode', chalk.green('✓'), app.get('httpsPort'), app.get('env'));
+//   console.log('  Press CTRL-C to stop\n');
+// });
 
 module.exports = app;
