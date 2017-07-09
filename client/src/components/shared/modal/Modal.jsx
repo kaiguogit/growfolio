980019afbe627d997c729643eab03bac59305b0f
Kai Guo <git@kaiguo.ca>
2017-07-08 17:14:00 -0700

    Use isolated scroll to prevent modal scroll from populate to parent

--- client/src/components/shared/modal/Modal.jsx
+++ client/src/components/shared/modal/Modal.jsx
@@ -2,12 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import ReactModal from 'react-modal';
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import isolatedScroll from 'isolated-scroll';
+import $ from 'jquery';
 
 ReactModal.setAppElement('#app');
 
 const Modal = (props) => {
-    const handleSubmit = () => {
+    const onAfterOpen = () => {
+        const modals = $('.modal-dialog');
+        if (props.isOpen && modals.length) {
+            modals.each((inx, modal) => {
+                isolatedScroll(modal);
+            });
+        }
+    }
+
+    const handleSubmit = () =>{
         let result = props.onSubmit();
         if (result instanceof Promise || typeof result.then === 'function') {
             result.then(props.onClose, props.onClose);
@@ -22,44 +32,37 @@ const Modal = (props) => {
             contentLabel={props.contentLabel}
             onRequestClose={props.onClose}
             closeTimeoutMS={300}
+            onAfterOpen={onAfterOpen}
             // modal modal-dialog classes are from bootstrap _modal.scss
             className={{
                 base: 'modal-dialog',
-                beforeClose: 'modal-show-leave-active'
+                beforeClose: 'modal-dialog-leave',
+                afterOpen: 'modal-dialog-show'
             }}
             overlayClassName={{
-                base: 'modal fade',
-                afterOpen: 'show'
+                base: 'modal fade react-modal',
             }}
         >
-            <ReactCSSTransitionGroup
-                transitionName="modal-show"
-                transitionAppear={true}
-                transitionAppearTimeout={300}
-                transitionEnter={false}
-                transitionLeave={false}
-            >
-                <div className="modal-content">
-                    <div className="modal-header">
-                        <h5 className="modal-title">{props.contentLabel}</h5>
-                        <button type="button" className="close mod-bare" aria-label="Close"
-                            onClick={props.onClose}>
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div className="modal-body">
-                        {props.children}
-                    </div>
-                    <div className="modal-footer">
-                        <button type="button" className="btn mod-secondary" onClick={props.onClose}>
-                            Cancel
-                        </button>
-                        <button type="button" className="btn mod-danger" onClick={handleSubmit} disabled={props.isFetching}>
-                            Yes
-                        </button>
-                    </div>
+            <div className="modal-content">
+                <div className="modal-header">
+                    <h5 className="modal-title">{props.contentLabel}</h5>
+                    <button type="button" className="close mod-bare" aria-label="Close"
+                        onClick={props.onClose}>
+                        <span aria-hidden="true">&times;</span>
+                    </button>
                 </div>
-            </ReactCSSTransitionGroup>
+                <div className="modal-body">
+                    {props.children}
+                </div>
+                <div className="modal-footer">
+                    <button type="button" className="btn mod-secondary" onClick={props.onClose}>
+                        Cancel
+                    </button>
+                    <button type="button" className="btn mod-danger" onClick={handleSubmit} disabled={props.isFetching}>
+                        Yes
+                    </button>
+                </div>
+            </div>
         </ReactModal>
     );
 };
