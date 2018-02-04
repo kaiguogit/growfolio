b76426eeddc75c85dbcffbb7703ebb9056c2a72f
Kai Guo <kguo@fortinet.com>
2018-02-04 09:58:40 -0800

    Set 2015 historical rate

--- client/src/reducers/initialState.js
+++ client/src/reducers/initialState.js
@@ -25,10 +25,12 @@ export default {
     currency: {
         isFetching: false,
         rate: [],
+        // https://help.simpletax.ca/questions/annual-exchange-rates?version=2015
         annualAverageRate: {
             USD: {
                 2017: 1.2986,
-                2016: 1.2986
+                2016: 1.324806,
+                2015: 1.278711
             }
         },
         // TODO support CRUD watch list
