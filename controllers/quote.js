7b76475b77be59837142894aeddeea3ebc55abca
kaiguogit <git@kaiguo.ca>
2018-04-04 11:06:39 -0700

    Add indent 4 in quote.js file

--- controllers/quote.js
+++ controllers/quote.js
@@ -9,21 +9,22 @@ const Quote = require('../models/Quote.js');
 
 
 const handleError = (err) => {
-  console.log(err);
+    console.log(err);
 };
 
 exports.getHistoricalQuotes = (req, res) => {
-  const date = req.query.date;
-  Quote.find({ _user: req.user._id,
-    date
-  })
-  .exec((err, data) => {
-    if (err) {
-      res.json({ result: [], error: err });
-    } else {
-      res.json({ result: data });
-    }
-  });
+    const date = req.query.date;
+    Quote.find({
+        _user: req.user._id,
+        date
+    })
+        .exec((err, data) => {
+            if (err) {
+                res.json({ result: [], error: err });
+            } else {
+                res.json({ result: data });
+            }
+        });
 };
 
 const isString = value => (typeof value === 'string');
@@ -38,144 +39,144 @@ const isValidNum = value => num(value) >= 0;
  *                   errors tips, and a global message for the whole form.
  */
 const validateQuoteForm = (payload) => {
-  const errors = {};
-  let isFormValid = true;
-  let message = '';
+    const errors = {};
+    let isFormValid = true;
+    let message = '';
 
-  if (!payload) {
+    if (!payload) {
+        return {
+            success: false,
+            errors: {},
+            message: 'Form is empty'
+        };
+    }
+
+    if (!isString(payload.symbol) || payload.symbol.trim().length === 0) {
+        isFormValid = false;
+        errors.symbol = 'Please provide ticker symbol.';
+    }
+
+    if (!isString(payload.date) || payload.date.trim().length === 0) {
+        isFormValid = false;
+        // TODO more robust date validation.
+        // Currently only expect DD-MM-YYYY
+        errors.symbol = 'Please provide date.';
+    }
+
+    if (!isString(payload.currency) || (payload.currency !== 'USD' &&
+        payload.currency !== 'CAD')) {
+        isFormValid = false;
+        errors.currency = 'Currency can only be CAD or USD';
+    }
+
+    if (!isValidNum(payload.price)) {
+        isFormValid = false;
+        errors.amount = 'Price is invalid';
+    }
+
+    if (!isValidNum(payload.change)) {
+        isFormValid = false;
+        errors.amount = 'Change is invalid';
+    }
+
+    if (!isValidNum(payload.changePercent)) {
+        isFormValid = false;
+        errors.amount = 'Change percent is invalid';
+    }
+
+    if (!isFormValid) {
+        message = 'Check the form for errors.';
+    }
+
     return {
-      success: false,
-      errors: {},
-      message: 'Form is empty'
+        success: isFormValid,
+        message,
+        errors
     };
-  }
-
-  if (!isString(payload.symbol) || payload.symbol.trim().length === 0) {
-    isFormValid = false;
-    errors.symbol = 'Please provide ticker symbol.';
-  }
-
-  if (!isString(payload.date) || payload.date.trim().length === 0) {
-    isFormValid = false;
-    // TODO more robust date validation.
-    // Currently only expect DD-MM-YYYY
-    errors.symbol = 'Please provide date.';
-  }
-
-  if (!isString(payload.currency) || (payload.currency !== 'USD' &&
-    payload.currency !== 'CAD')) {
-    isFormValid = false;
-    errors.currency = 'Currency can only be CAD or USD';
-  }
-
-  if (!isValidNum(payload.price)) {
-    isFormValid = false;
-    errors.amount = 'Price is invalid';
-  }
-
-  if (!isValidNum(payload.change)) {
-    isFormValid = false;
-    errors.amount = 'Change is invalid';
-  }
-
-  if (!isValidNum(payload.changePercent)) {
-    isFormValid = false;
-    errors.amount = 'Change percent is invalid';
-  }
-
-  if (!isFormValid) {
-    message = 'Check the form for errors.';
-  }
-
-  return {
-    success: isFormValid,
-    message,
-    errors
-  };
 };
 
 const returnError = (message, errors) => ({
-  success: false,
-  message,
-  errors
+    success: false,
+    message,
+    errors
 });
 
 const createOrEditQuotes = isEdit => (req, res) => {
-  const keys = ['symbol', 'currency', 'price', 'date', 'change', 'changePercent'];
-  const data = {};
-  keys.forEach((key) => {
-    data[key] = req.body[key];
-  });
-
-  const validationResult = validateQuoteForm(data);
-  if (!validationResult.success) {
-    return res.status(400).json(returnError(validationResult.message, validationResult.errors));
-  }
-
-  data._user = req.user._id;
-
-  if (!isEdit) {
-    new Quote(data).save((err, savedTsc) => {
-      return res.json({ result: savedTsc });
+    const keys = ['symbol', 'currency', 'price', 'date', 'change', 'changePercent'];
+    const data = {};
+    keys.forEach((key) => {
+        data[key] = req.body[key];
     });
-  } else {
-    Quote.findById(req.body._id, (error, tsc) => {
-      if (error) {
-        return res.status(400).json(returnError(`Cannot find transaction ${req.body._id}`, error));
-      }
 
-      Object.assign(tsc, data);
-      tsc.save((error, updatedTsc) => {
-        if (error) {
-          return res.status(400).json(returnError(`Cannot update transaction ${req.body._id}`, error));
-        }
-        return res.json({ result: updatedTsc });
-      });
-    });
-  }
+    const validationResult = validateQuoteForm(data);
+    if (!validationResult.success) {
+        return res.status(400).json(returnError(validationResult.message, validationResult.errors));
+    }
+
+    data._user = req.user._id;
+
+    if (!isEdit) {
+        new Quote(data).save((err, savedTsc) => {
+            return res.json({ result: savedTsc });
+        });
+    } else {
+        Quote.findById(req.body._id, (error, tsc) => {
+            if (error) {
+                return res.status(400).json(returnError(`Cannot find transaction ${req.body._id}`, error));
+            }
+
+            Object.assign(tsc, data);
+            tsc.save((error, updatedTsc) => {
+                if (error) {
+                    return res.status(400).json(returnError(`Cannot update transaction ${req.body._id}`, error));
+                }
+                return res.json({ result: updatedTsc });
+            });
+        });
+    }
 };
 
 exports.createHistoricalQuotes = createOrEditQuotes(false);
 exports.editHistoricalQuotes = createOrEditQuotes(true);
 
 exports.deleteHistoricalQuotes = (req, res) => {
-  Quote.remove({ _id: req.body.id, _user: req.user._id }, (err) => {
-    if (err) return handleError(err);
-    // removed!
-    res.json({ result: { message: 'removed' } });
-  });
+    Quote.remove({ _id: req.body.id, _user: req.user._id }, (err) => {
+        if (err) return handleError(err);
+        // removed!
+        res.json({ result: { message: 'removed' } });
+    });
 };
 
 
 const BASE_URI = 'https://www.alphavantage.co/query';
 // Expect symbol for TSX to have prefix "TSX:", e.g TSX:VFV
 const makeDailyQuotesUrl = (symbol) => {
-  const params = {
-    function: 'TIME_SERIES_DAILY',
-    symbol,
-    apikey: process.env.ALPHA_VANTAGE_API_KEY
-  };
-  return makeUrl(BASE_URI, params);
+    const params = {
+        function: 'TIME_SERIES_DAILY',
+        symbol,
+        apikey: process.env.ALPHA_VANTAGE_API_KEY
+    };
+    return makeUrl(BASE_URI, params);
 };
 
 const normalizeAPIResult = (response) => {
-  const result = {};
-  ['1. open', '2. high', '3. low', '4. close', '5. volume'].forEach((key) => {
-    const normalKey = key.split('. ')[1];
-    result[normalKey] = response[key];
-  });
-  return result;
+    const result = {};
+    ['1. open', '2. high', '3. low', '4. close', '5. volume'].forEach((key) => {
+        const normalKey = key.split('. ')[1];
+        result[normalKey] = response[key];
+    });
+    return result;
 };
 
 const saveQuotes = (res, response) => {
     const meta = response['Meta Data'];
     const quotes = response['Time Series (Daily)'];
     if (response.Information) {
-      res.json({
-        status_code: 403,
-        message: response.Information,
-        success: false
-      });
+        res.json({
+            status_code: 403,
+            message: response.Information,
+            success: false
+        });
     }
     if (meta && quotes) {
         let symbol = meta['2. Symbol'];
@@ -188,15 +189,13 @@ const saveQuotes = (res, response) => {
         Object.keys(quotes).forEach((date) => {
             const data = normalizeAPIResult(quotes[date]);
             Quote.find({ date, symbol }).exec().then((foundQuote) => {
-                if (error) {
-                    new Quote(data).save((err, savedQuote) => {
-                        result[date] = savedQuote;
-                    });
-                }
-           }, error => {
-            
-            console.log(error);
-           });
+                new Quote(data).save((err, savedQuote) => {
+                    result[date] = savedQuote;
+                });
+            }, error => {
+
+                console.log(error);
+            });
         });
         res.json({ success: true, result });
     }
@@ -206,27 +205,27 @@ const saveQuotes = (res, response) => {
  * GET /api/download-historical-quotes
  */
 exports.downloadHistoricalQuotes = (req, res) => {
-  const symbol = req.query.symbol;
-  const url = makeDailyQuotesUrl(symbol);
-  saveQuotes(res, fakeData);
-  // request(url, (error, response, body) => {
-  //   const errorReponse = {
-  //     status_code: request.statusCode,
-  //     error,
-  //     message: 'Error when getting quotes',
-  //     success: false
-  //   };
-  //   if (error) { res.json(errorReponse); }
-  //   // if (request.statusCode === 403) {
-  //   //   return next(new Error('Error when getting quotes'));
-  //   // }
-  //   // Google returns string with //, chop it off.
-  //   try {
-  //     // const result = JSON.parse(body.replace(/\/\//, ''));
-  //     const result = JSON.parse(body);
-  //     saveQuotes(res, result);
-  //   } catch (err) {
-  //     res.json(errorReponse);
-  //   }
-  // });
+    const symbol = req.query.symbol;
+    const url = makeDailyQuotesUrl(symbol);
+    saveQuotes(res, fakeData);
+    // request(url, (error, response, body) => {
+    //   const errorReponse = {
+    //     status_code: request.statusCode,
+    //     error,
+    //     message: 'Error when getting quotes',
+    //     success: false
+    //   };
+    //   if (error) { res.json(errorReponse); }
+    //   // if (request.statusCode === 403) {
+    //   //   return next(new Error('Error when getting quotes'));
+    //   // }
+    //   // Google returns string with //, chop it off.
+    //   try {
+    //     // const result = JSON.parse(body.replace(/\/\//, ''));
+    //     const result = JSON.parse(body);
+    //     saveQuotes(res, result);
+    //   } catch (err) {
+    //     res.json(errorReponse);
+    //   }
+    // });
 };
